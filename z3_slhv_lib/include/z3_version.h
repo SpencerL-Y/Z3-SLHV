e8ad91bd9f70e4ef702020a8fc317d1905980d11
clexmaAtFlankerB <lixie19@ios.ac.cn>
2024-08-08 11:05:52 +0800

    add gitignore

--- z3_slhv_lib/include/z3_version.h
+++
@@ -1,8 +0,0 @@
-// automatically generated file.
-#define Z3_MAJOR_VERSION   4
-#define Z3_MINOR_VERSION   12
-#define Z3_BUILD_NUMBER    2
-#define Z3_REVISION_NUMBER 0
-
-#define Z3_FULL_VERSION    "4.12.2.0 236b0e00ba060684f67c98a5aaaaa17314e00ad3"
-#define Z3GITHASH 236b0e00ba060684f67c98a5aaaaa17314e00ad3
