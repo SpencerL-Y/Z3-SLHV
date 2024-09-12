525f4809a5d776ac1820575fca27237b47bbc58b
clexmaAtFlankerB <lixie19@ios.ac.cn>
2024-09-12 12:08:09 +0800

    update

--- src/util/slhv_debug.h
+++ src/util/slhv_debug.h
@@ -2,26 +2,21 @@
 
 // #define SLHV_DEBUG
 
-
 // debug macro
 // #define SLHV_PRINT
-// // #define DED_INFO
+// #define DED_INFO
 // #define MODEL_GEN_INFO
 // // #define SLHV_UNSAT_CORE_DEBUG
 
-// // minimal for debug
 // #define SOLVING_INFO
-
-// // for new encoding 
+// #define ASSERTION_DEBUG
+// #define HTERM_DEBUG
 // #define DISJ_DEBUG
 
-// for inf graph debug
 // #define INF_GRAPH_DEBUG
 
-// for readwrite support
 // #define SLHV_RW_DEBUG
 
-// for disjh and subh support
 // #define SLHV_HTR_DEBUG
 
 // #define SLHV_ARRAY_DEBUG
