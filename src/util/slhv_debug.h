8293a705622a42aea3d4a034754fd67eb1f8ca0e
clexmaAtFlankerB <lixie19@ios.ac.cn>
2024-09-30 16:18:58 +0800

    update

--- src/util/slhv_debug.h
+++ src/util/slhv_debug.h
@@ -1,24 +1,24 @@
-#pragma once
+// #pragma once
 
 // #define SLHV_DEBUG
 
-// debug macro
+// // debug macro
 // #define SLHV_PRINT
-// #define DED_INFO
-// #define MODEL_GEN_INFO
-// // #define SLHV_UNSAT_CORE_DEBUG
+// // #define DED_INFO
+// // #define MODEL_GEN_INFOs
+// // // #define SLHV_UNSAT_CORE_DEBUG
 
 // #define SOLVING_INFO
-// #define ASSERTION_DEBUG
-// #define HTERM_DEBUG
+// // #define ASSERTION_DEBUG
+// // #define HTERM_DEBUG
 // #define DISJ_DEBUG
 
-// #define INF_GRAPH_DEBUG
+// // #define INF_GRAPH_DEBUG
 
-// #define SLHV_RW_DEBUG
+// // #define SLHV_RW_DEBUG
 
-// #define SLHV_HTR_DEBUG
+// // #define SLHV_HTR_DEBUG
 
-// #define SLHV_ARRAY_DEBUG
+// // #define SLHV_ARRAY_DEBUG
 
-// frontend macro
+// // frontend macro
