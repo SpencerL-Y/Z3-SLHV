f21fdc713716d025d38d41ea37419ff750c72fd2
clexmaAtFlankerB <lixie19@ios.ac.cn>
2024-10-12 18:07:14 +0800

    update scripts

--- shared_recompile_install.py
+++ shared_recompile_install.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.system("cd ./build && ninja install && cd ../z3_slhv_lib")
+    os.system("cd ./build && ninja install")
     # && sudo cp -rf * /usr/local/")
--- static_recompile_install.py
+++ static_recompile_install.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.system("cd ./build && make -j16 && cd ../z3_slhv_lib && make install") 
+    os.system("cd ./build && make -j16 && make install") 
     # && sudo cp -rf * /usr/local/")
